c62bdda3e8cedaee12d0652a715e4a29896286ba
Mark Lyons <mark.lyons@imaginelearning.com>
2022-06-29 15:09:29 -0400

    Implement `parallelizeWorkTasks`

--- src/parallelizeWorkTasks.ts
+++ src/parallelizeWorkTasks.ts
@@ -1,13 +1,36 @@
 /**
  *
  * @param {Promise[]} promises
- * @param {Number} parallelExecutionMax
- * @desc Given `Promises[]`
+ * @param {number} parallelExecutionMax
+ * @desc Given `Promises[]`, process all of the promises without the amount of current processes exceeding the `parallelExecutionMax` count.
  *
  */
 
-export function parallelizeWorkTasks(promises: Array<() => Promise<string>>, parallelExecutionMax: number) : Promise<string>[] {
+export async function parallelizeWorkTasks(promises: Array<() => Promise<string>>, parallelExecutionMax: number) {
+    // we need to return the results in order, so create an array for that. 
+    const results: string[] = [];
 
-    throw new Error("Not Implemented");
+    // we run the first `parallelExecutionMax` amount jobs right away
+    // so initialize at the count
+    let curIdx = parallelExecutionMax;
 
+    const initialPromises = promises.slice(0, parallelExecutionMax);
+
+    await Promise.all(initialPromises.map((promise, idx) => worker(promise, idx)));
+
+    return Promise.resolve(results);
+    
+    /**
+     * 
+     * @param {Promise<string>} job job to process
+     * @param {number} idx current job's index
+     */
+    async function worker(job: () => Promise<string>, idx: number){
+        const result = await job();
+
+        // save off results to correct index in `results`
+        results[idx] = result;
+        // if all haven't been processed, process the next one
+        if(curIdx < promises.length) await worker(promises[curIdx], curIdx++);
+    }
 }
