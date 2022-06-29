ea319ea396e37dd0e7522e837c888ccefef1707e
Mark Lyons <mark.lyons@imaginelearning.com>
2022-06-29 15:08:39 -0400

    Update accountClient.ts

--- src/accountClient.ts
+++ src/accountClient.ts
@@ -1,7 +1,16 @@
 import {Users} from './types/users';
 import db from "./databases/users.json";
 
+/**
+ * 
+ */
 export default class AccountClient {
+    /**
+     * 
+     * @param {string} email 
+     * @param {string} password 
+     * @returns {User} the account object
+     */
     async login(email: string, password: string) {
         return new Promise((resolve, reject) => {
             const user = (<Users>db)[`${email}:${password}`];
